d48ba33a07d72f5d12a20f56e34e86bc4546c2de
jihee Kim <jkim541@myseneca.ca>
2024-04-30 16:11:56 -0400

    Folder Structure

--- src/sections/floating-nav/FloatingNav.jsx
+++ src/sections/floating-nav/FloatingNav.jsx
@@ -2,7 +2,7 @@ import './floating-nav.css'
 
 const FloatingNav = () => {
   return (
-    <section id="floating__nav"></section>
+    <section id="floating__nav"><h2>FloatingNav</h2></section>
   )
 }
 
